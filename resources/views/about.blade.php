87e4fb8a4b7895021f3180738a5978cdfb2da6c0
Phanurat <paunavt5@gmail.com>
2023-11-07 10:21:08 +0700

    Add Blade file about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Me</title>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, molestias ea. Deleniti iste at est nihil dignissimos omnis natus voluptatibus assumenda molestiae repellendus vero maxime, unde ex, eius suscipit eligendi?</p>
+    
+</body>
+</html>
