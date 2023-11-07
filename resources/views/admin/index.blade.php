12ac2f78b61320fe549910e4d8ba5a5032774e7c
Phanurat <paunavt5@gmail.com>
2023-11-07 10:45:56 +0700

    php in file blade

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,8 +7,19 @@
     <title>Admin</title>
 </head>
 <body>
-    <h1>Welcome Admin</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium labore, officiis, fuga omnis modi ratione, hic mollitia vel distinctio iusto quae. Ipsam nobis quia nulla cupiditate quos eligendi facere repellendus!</p>
+    <?php 
+        $user = "Phanurat";
+    ?>
     
+
+    @if($user == "Phanurat")
+        <h1>User is Admin</h1>
+        <h1>Welcome Admin</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium labore, officiis, fuga omnis modi ratione, hic mollitia vel distinctio iusto quae. Ipsam nobis quia nulla cupiditate quos eligendi facere repellendus!</p>
+        <h1>{{$user}}</h1>
+    @else 
+        <h1>User isn't admin</h1>
+    @endif
+
 </body>
 </html>
